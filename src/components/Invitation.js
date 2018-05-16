634a4cb2f4facdb0c6562bae36f0b4ec1d6e5cd5
Vicky Lau <vlau.nyc@gmail.com>
2018-05-16 19:14:36 +0000

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,12 @@
-// Code Invitation Component Here
+import React from 'react';
+
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <div className="invite">
+        <div className="invite-title"><h1>You&#39;ve been invited!</h1></div>
+        <div className="invite-body">{this.props.children}</div>
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,15 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecorations extends React.Component {
+  render () {
+    const childrenWithThemeProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <div>{childrenWithThemeProp}</div>
+    );
+  }
+}
